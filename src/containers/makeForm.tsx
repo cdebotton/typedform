851db197aadb24f479699ab405b7ace454fd6ffd
Christian de Botton <cdebotton@gmail.com>
2017-11-22 22:36:39 -0800

    Add test

--- src/containers/makeForm.tsx
+++ src/containers/makeForm.tsx
@@ -1,9 +1,28 @@
-import React from 'react';
+import * as React from 'react';
 
-export type Props<T> = {
+type Props<T> = {
   initialValue?: T;
 };
 
-export function makeForm<T>(_: new () => T) {
-  return class extends React.Component<Props<T>> {};
+type State<T> = {
+  fields: {
+    [K in keyof T]: {
+      value: T[K];
+      isDirty: boolean;
+      isValid: boolean;
+      update: (value: T[K]) => void;
+    }
+  };
+};
+
+export function makeForm<T>(schema: new () => T) {
+  if (!schema) {
+    throw new ReferenceError('You must provide a valid form schema');
+  }
+
+  return class extends React.Component<Props<T>, State<T>> {
+    public constructor(props: Props<T>) {
+      super(props);
+    }
+  };
 }
